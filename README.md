1fca04cd2c378a35c01e5eece9a60df2e4931642
Alex <alexjoelaid97@gmail.com>
2024-10-02 11:24:21 +0200

    added map view

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ EXPO SDK
 
 - https://docs.expo.dev/versions/latest/sdk/contacts/
 - https://docs.expo.dev/versions/latest/sdk/battery/
+- https://docs.expo.dev/versions/latest/sdk/map-view/
--- navigators/TabNavigator.tsx
+++ navigators/TabNavigator.tsx
@@ -2,11 +2,13 @@ import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import CardScreen from "../screens/CardScreen";
 import GameScreen from "../screens/GameScreen";
+import MapScreen from "../expo-components/MapScreen";
 
 //Beskriv vad navigatorn ska innehålla
 export type TabParamList = {
   CardScreen: undefined;
   GameScreen: undefined;
+  MapScreen: undefined;
 };
 //Skapar navigatorn från TabParamList
 const Tab = createBottomTabNavigator<TabParamList>();
@@ -28,6 +30,7 @@ export default function TabNavigator() {
     >
       <Tab.Screen name="GameScreen" component={GameScreen} />
       <Tab.Screen name="CardScreen" component={CardScreen} />
+      <Tab.Screen name="MapScreen" component={MapScreen} />
     </Tab.Navigator>
   );
 }
