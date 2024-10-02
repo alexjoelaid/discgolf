83fc45b1ead0965c04e5a1a0129701dc6432cb52
Alex <alexjoelaid97@gmail.com>
2024-10-02 11:31:37 +0200

    added so the map gets my current location

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ EXPO SDK
 - https://docs.expo.dev/versions/latest/sdk/contacts/
 - https://docs.expo.dev/versions/latest/sdk/battery/
 - https://docs.expo.dev/versions/latest/sdk/map-view/
+- https://docs.expo.dev/versions/latest/sdk/location/
--- expo-components/MapScreen.tsx
+++ expo-components/MapScreen.tsx
@@ -1,11 +1,57 @@
-import React from "react";
-import MapView from "react-native-maps";
-import { StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Alert } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function MapScreen() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  if (errorMsg) {
+    Alert.alert("Error", errorMsg);
+  }
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        showsUserLocation={true}
+        region={
+          location
+            ? {
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }
+            : undefined
+        }
+      >
+        {location && (
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            title="You are here"
+          />
+        )}
+      </MapView>
     </View>
   );
 }
