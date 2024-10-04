056b6a63af0a5b8e5d81fa46665b4eec8a901ffe
Alex <alexjoelaid97@gmail.com>
2024-10-04 09:08:53 +0200

    changed name

--- expo-components/MapScreen.tsx
+++ expo-components/MapScreen.tsx
@@ -13,7 +13,7 @@ export default function MapScreen() {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
+        setErrorMsg("access denied");
         return;
       }
 
